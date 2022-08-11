fbc1a82acd646d7848a3067d89f583885ae30658
beyondGnE <51811042+beyondGnE@users.noreply.github.com>
2022-08-11 10:57:10 -0600

    Removed class files

--- driver.java
+++ driver.java
@@ -1,6 +1,6 @@
 import com.eugenehk.chess.Chess;
 
-public class driver {
+public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         byte value = 25;
